164aeda5bd04edb3e3aeb238f2dea87f32de5745
Rahat Ibn Rafiq <rahatibnrafiq@Rahats-MacBook-Pro.local>
2022-05-28 11:23:47 -0700

    move theme codes from main.dart to globals.dart

--- walk_with_me/lib/globals/globals.dart
+++ walk_with_me/lib/globals/globals.dart
@@ -1,5 +1,7 @@
 // ignore: non_constant_identifier_names
-// ignore_for_file: non_constant_identifier_names, duplicate_ignore
+// ignore_for_file: non_constant_identifier_names, duplicate_ignore, unnecessary_const
+
+import 'package:flutter/material.dart';
 
 final List<String> BACKGROUND_CITY_IMAGES = [
   "barcelona.jpg",
@@ -11,5 +13,44 @@ final List<String> BACKGROUND_CITY_IMAGES = [
   "paris.jpg",
 ];
 
+const String appTitle = "Walk With Me";
+
 // ignore: constant_identifier_names
 const String CAROUSEL_IMAGES_ADDRESS = "assets/images/";
+
+final ThemeData themeData = ThemeData(
+  primaryColor: const Color(0xFF3EBACE),
+  scaffoldBackgroundColor: const Color(0xFFF3F5F7),
+  brightness: Brightness.light,
+  fontFamily: 'Georgia',
+  textTheme: const TextTheme(
+    headline1: TextStyle(
+      fontSize: 35.0,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFF1F1A30),
+    ),
+    headline2: TextStyle(
+      fontSize: 30.0,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFF1F1A30),
+    ),
+    headline3: TextStyle(
+      fontSize: 25.0,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFF1F1A30),
+    ),
+    headline4: TextStyle(
+      fontSize: 16.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
+    bodyText1: TextStyle(
+      fontSize: 16.0,
+      color: Colors.purple,
+    ),
+    bodyText2: TextStyle(
+      fontSize: 16.0,
+      color: Color(0xFF1F1A30),
+    ),
+  ),
+);
--- walk_with_me/lib/main.dart
+++ walk_with_me/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:walk_with_me/globals/globals.dart';
 import 'package:walk_with_me/screens/signin.dart';
 
 void main() {
@@ -8,49 +9,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Walk With Me',
-      theme: ThemeData(
-        primaryColor: const Color(0xFF3EBACE),
-        scaffoldBackgroundColor: const Color(0xFFF3F5F7),
-        brightness: Brightness.light,
-        fontFamily: 'Georgia',
-        textTheme: const TextTheme(
-          headline1: TextStyle(
-            fontSize: 35.0,
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF1F1A30),
-          ),
-          headline2: TextStyle(
-            fontSize: 30.0,
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF1F1A30),
-          ),
-          headline3: TextStyle(
-            fontSize: 25.0,
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF1F1A30),
-          ),
-          headline4: TextStyle(
-            fontSize: 16.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-          bodyText1: TextStyle(
-            fontSize: 16.0,
-            color: Colors.purple,
-          ),
-          bodyText2: TextStyle(
-            fontSize: 16.0,
-            color: Color(0xFF1F1A30),
-          ),
-        ),
-      ),
+      title: appTitle,
+      theme: themeData,
       home: const Signin(),
     );
   }
